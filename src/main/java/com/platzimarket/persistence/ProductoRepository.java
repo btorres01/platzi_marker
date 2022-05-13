69cd5a78a742f520371302cb73a5acda9509405c
Brahian Torres <brahiant0@gmail.com>
2022-05-13 17:03:32 -0500

    Se organiza el dominio

---
+++ src/main/java/com/platzimarket/persistence/ProductoRepository.java
@@ -0,0 +1,37 @@
+package com.platzimarket.persistence;
+
+import com.platzimarket.persistence.crud.ProductoCrudRepository;
+import com.platzimarket.persistence.entity.Producto;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public class ProductoRepository {
+   private ProductoCrudRepository productoCrudRepository;
+
+   public List<Producto> getAll(){
+       return (List<Producto>) productoCrudRepository.findAll();
+    }
+
+    public List<Producto> getByCategoria(int idCategoria){
+       return productoCrudRepository.findByIdCategoriaOrderByNombreAsc(idCategoria);
+    }
+
+    public Optional<List<Producto>> getEscasos(int cantidadStock){
+       return  productoCrudRepository.findByCantidadStockLessThanAAndEstado(cantidadStock,true);
+    }
+
+    public Optional<Producto> getProducto(int idProducto){
+       return  productoCrudRepository.findById(idProducto);
+    }
+
+    public Producto save(Producto producto){
+       return productoCrudRepository.save(producto);
+    }
+
+    public void delete(int idProducto){
+       productoCrudRepository.deleteById(idProducto);
+    }
+}
---
+++ src/main/java/com/platzimarket/persistence/entity/ComprasProductoPK.java
@@ -0,0 +1,41 @@
+package com.platzimarket.persistence.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Embeddable;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import java.io.Serializable;
+
+@Embeddable
+public class ComprasProductoPK implements Serializable {
+
+    @Column(name = "id_compra")
+    private Integer idCompra;
+
+    @Column(name = "id_producto")
+    private Integer idProducto;
+
+    @ManyToOne
+    @JoinColumn(name = "id_compra", insertable = false, updatable = false)
+    private Compra compra;
+
+    @ManyToOne
+    @JoinColumn(name = "id_producto", insertable = false, updatable = false)
+    private Producto producto;
+
+    public Integer getIdCompra() {
+        return idCompra;
+    }
+
+    public void setIdCompra(Integer idCompra) {
+        this.idCompra = idCompra;
+    }
+
+    public Integer getIdProducto() {
+        return idProducto;
+    }
+
+    public void setIdProducto(Integer idProducto) {
+        this.idProducto = idProducto;
+    }
+}
